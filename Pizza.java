6beacdcf378cd5099e181fc0ef29bf3b292aabc6
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-02-28 20:30:52 -0300

    alterando a classe pizza, e alterando o diagrama

--- Pizza.java
+++ Pizza.java
@@ -1,17 +1,49 @@
 public class Pizza {
     /*
+     * Feito por: ERIC RODRIGUES DINIZ
+     * MATRÍCULA: 707760
+     * 🍕Exercício 01: Conceitos básicos de POO 🍕
+     * Uma pizzaria precisa de um sistema de software para controlar suas vendas. O
+     * primeiro passo para isso é automatizar o valor cobrado pelas pizzas vendidas.
+     * A pizzaria está iniciando os negócios com um modelo simplificado de vendas,
+     * seguindo estas regras:
+     *
+     * A pizza básica contém queijo e calabresa, custando R$25.
+     * A pizza pode ser personalizada com até 8 ingredientes adicionais. Já que
+     * todos têm o mesmo valor, não é necessário diferenciar os tipos ou nomes de
+     * adicionais. Cada ingrediente adicional custa R$4.
+     * Cada pizza vendida deve gerar uma nota de compra simplificada, contendo
+     * descrição e valor a ser pago pela pizza.
+     * Sua tarefa inicial é analisar o problema e projetar uma classe Pizza que
+     * atenda aos requisitos levantados. Para criar o modelo, utilize uma das
+     * ferramentas indicadas na página inicial da disciplina.
+     *
+     * ATENÇÃO PARA O COMPLEMENTO DA TAREFA:
+     *
+     * Para terminar a primeira fase do projeto, escreva o código para sua classe
+     * Pizza. Você precisa ter certeza que a sua classe está correta. Então, escreva
+     * uma classe de teste unitário utilizando JUnit, verificando a correção da
+     * classe Pizza.
+     */
+
+    /*
      * AQUI CONTÉM A CLASSE PIZZA
      */
     private int id;
     private int qtd_adicionais;
-    private static final double valor_fixo = 25;
-    private static final double valor_adicionais = 4;
+    private static final double valor_fixo = 25; // atributo de classe
+    private static final double valor_adicionais = 4; // atributo de classe
 
     // CONSTRUCTORS
     public Pizza() {
 
     }
 
+    /**
+     * @param qtd_adicionais colocar como parametro apenas a quantidade de
+     *                       adicionais, e irá retornar um objeto com o param
+     *                       inserido
+     */
     public Pizza(int qtd_adicionais) {
         this.qtd_adicionais = qtd_adicionais;
     }
@@ -46,6 +78,9 @@ public class Pizza {
         return this.getValor_fixo() + (this.getQtd_adicionais() * valor_adicionais);
     }
 
+    /**
+     * @return sem parametros, irá retornar a nota com a descrição e valor final
+     */
     public void gerar_Nota() {
         System.out.println(
                 "O valor final da pizza de calabresa e queijo é: " + valor_Final() + " e contém "
--- PizzaTeste.java
+++ PizzaTeste.java
@@ -1,41 +1,61 @@
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
 
 class PizzaTeste {
 
     public Pizza pizza = new Pizza();
     public Pizza pizzaSemAdicionais = new Pizza(0);
-    public Pizza pizzaComAdicionais = new Pizza(7);
+    public Pizza pizzaComAdicionais = new Pizza(8);
     public Pizza pizzaComAdicionaisAlemMaximoPermitido = new Pizza(9);
 
+    /**
+     * Teste para valor fixo da pizza, expectativa igual 25
+     */
     @Test
     void valorFixoTemQueSerIgualVinteCinco() {
         assertEquals(25.00, pizzaComAdicionais.getValor_fixo());
     }
 
+    /**
+     * Teste para qtd do adicionais no max 8
+     */
     @Test
     void maxDeAdicionaisIgualOito() {
-        pizzaComAdicionais.setQtd_adicionais(7);
-        assertSame(7, pizzaComAdicionais.getQtd_adicionais());
+        assertSame(8, pizzaComAdicionais.getQtd_adicionais());
     }
 
+    /**
+     * Teste para objeto pizza sem adicionais ou igual a 0
+     */
     @Test
     void pizzaSemAdicionais() {
-        pizzaSemAdicionais.getQtd_adicionais();
-
+        assertEquals(0, pizzaSemAdicionais.getQtd_adicionais());
     }
 
-    // gerar nota final
+    /**
+     * Teste para gerar nota com descrição e valor final da pizza
+     */
     @Test
     void gerarNota() {
-
+        // assertEq("O valor final da pizza de calabresa e queijo é: 25 e contém 0
+        // adicionais", pizzaSemAdicionais.gerar_Nota());
+        String descricao = "O valor final da pizza de calabresa e queijo é: 25 e contém 0 adicionais";
+        // assertTrue(descricao.equals(pizzaSemAdicionais.gerar_Nota()), true);
+        // assertEquals(descricao, pizzaSemAdicionais.gerar_Nota());
     }
 
-    // gerar valor final
+    /**
+     * Teste para gerar valor final após passar a qtd do adicionais
+     */
     @Test
     void gerarValorFinal() {
-        assertEquals(53, pizzaComAdicionais.valor_Final());
+        assertEquals(57, pizzaComAdicionais.valor_Final());
+        assertEquals(25, pizzaSemAdicionais.valor_Final());
     }
 }
