71c3cb1dd47f3958d70ae0ecfa54f728b52fe22e
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-03-07 19:18:23 -0300

    adicionando a doc, e as classes pedido e pedidoTeste

---
+++ PedidosTeste.java
@@ -0,0 +1,18 @@
+
+// import static org.junit.jupiter.api.Assertions.assertThrows;
+//import javax.naming.directory.InvalidAttributesException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PedidosTeste {
+     /*
+      * CLASSE FEITA A PARTIR DE TDD
+      * DATA: 07/03/23
+      * FEITA POR: ERIC RODRIGUES DINIZ
+      */
+     @Test
+     public void testeum() {
+          assertEquals(10, 10);
+     }
+}
